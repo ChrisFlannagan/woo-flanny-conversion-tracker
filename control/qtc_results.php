035850adae3f0884e2018d0e79fb9cee2d7f0bf4
ChrisFlannagan <chris@champoosa.com>
2016-10-07 17:24:41 -0400

    - working on admin - started QTC_RESULTS class - Should be pulling all orders of a tracking code but haven't tested

--- control/qtc_results.php
+++ control/qtc_results.php
@@ -5,3 +5,20 @@
  * Date: 10/7/16
  * Time: 3:07 PM
  */
+
+class QTC_RESULTS {
+	private $orders = array();
+
+	public function __construct( $tracking_code ) {
+		global $wpdb;
+		$prefix = $wpdb->get_prefix();
+		$order_ids = $wpdb->get_col( $wpdb->prepare( " 
+ 			SELECT post_id
+			FROM $wpdb->postmeta 
+			WHERE meta_key=%s",
+			$tracking_code ) );
+		foreach ( $order_ids as $id ) {
+			$orders[] = new WC_Order( $id );
+		}
+	}
+}
--- control/qtc_session.php
+++ control/qtc_session.php
@@ -5,6 +5,9 @@
  * This tracks the user visiting the page and when they convert
  */
 class QTC_SESSION {
+	/**
+	 * Initialize a session if there's a tracking link attached to the url
+	 */
 	public static function initialize() {
 		// Begin a session if not already going
 		if ( ! session_id() ) {
@@ -16,9 +19,15 @@ class QTC_SESSION {
 		}
 	}
 
+	/**
+	 * This funciton is called when the woocommerce_thankyou hook fires.  It means an order has just been
+	 * finalized and the hook passes in the $order_id param which we use to store post_meta of the tracking
+	 * code connected to the specific order
+	 * 
+	 * @param $order_id
+	 */
 	public static function record_conversion( $order_id ) {
 		if ( isset ( $_SESSION['qtc_woo_tracking_code'] ) ) {
-			$order = new WC_Order( $order_id );
 			update_post_meta( $order_id, "_qtc_woo_tracked_" . $_SESSION['qtc_woo_tracking_code'], 1 );
 		}
 	}
